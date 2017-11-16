3df28bcdef445312afd2c9b6e43a514607b3c1cc
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 13:33:53 -0800

    Display all shrooms, with badge on shared shrooms.

--- scripts/pages/view-myshrooms.js
+++ scripts/pages/view-myshrooms.js
@@ -1,14 +1,9 @@
 const userId = shroomUser.id
 const container = document.querySelector('#shroom-bento')
 
-// Get shrooms owned by user
-
-
-
-// Get shrooms shared with user and add badge
-
+// Display all shrooms
 const getShroomsPromises = [
-  getShroomsOwnedByUser(2),
+  getShroomsOwnedByUser(1),
   getShroomsSharedWithUser(2)
 ]
 Promise.all(getShroomsPromises)
@@ -17,6 +12,22 @@ Promise.all(getShroomsPromises)
   const attachPartsPromises = allShrooms.map(shroom => attachParts(shroom))
   Promise.all(attachPartsPromises)
   .then(result => {
-    console.log(result)
+    const sortedShrooms = sortShroomsByDate(result)
+    sortedShrooms.forEach(shroom => {
+      let newCard = document.createElement('div')
+      newCard.style = 'position: relative; display: inline-block; margin: 10px; width: 20%; min-width: 120px;'
+      newCard.innerHTML = makeShroomCard(shroom)
+      // Add badge to shrooms shared with user. These are those whose owner_id does not match the user.
+      if (shroom.owner_id !== userId) {
+        const badge = document.createElement('div')
+        badge.classList = 'badge badge-pill badge-success'
+        badge.style = 'position: absolute; top: 10px; right: 10px;'
+        badge.innerText = 's'
+        newCard.appendChild(badge)
+      }
+      container.appendChild(newCard)
+      // Add click listener to go to view-shroom page
+      newCard.addEventListener('click', () => window.location=`view-shroom.html#/shroom/${shroom.id}`)
+    })
   })
 })
