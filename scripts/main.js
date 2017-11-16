33dbd3b71682ef1e145a18f04be52c0b7132eb7f
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 10:44:20 -0800

    Move background image to body. Adjust resizing so it is fixed. Make navbar sticky at top. Adjust bottom of page spacing.

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,6 @@
 // Following line is to connect to locally hosted server
 const baseURL = 'http://localhost:3000'
 
-// Set background image size
-function resizeBkgImage() {
-  const main = document.querySelector('main')
-  const navbar = document.querySelector('.navbar')
-  let winHeight = window.innerHeight - navbar.clientHeight // subtract navbar height
-  main.style.height = `${winHeight}px`
-}
-resizeBkgImage() // on page load
-window.onresize = resizeBkgImage //on window resize
-
 // Check for logged in user
 let shroomUser = JSON.parse(localStorage.getItem('shroomUser'))
 // If user not logged in, redirect to intro page
