a0922b8e097598bb4106eac89c4fa596d7109473
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 20:57:14 -0800

    Rename resizeLargeShroomContainer to resizeShroomContainer.

--- scripts/pages/view-shroom.js
+++ scripts/pages/view-shroom.js
@@ -2,8 +2,8 @@ const shroomId = getShroomIdFromURL()
 const container = document.querySelector('#shroom-view')
 
 // Set shroom card image size
-resizeLargeShroomContainer() // on page load
-window.onresize = resizeLargeShroomContainer //on window resize
+resizeShroomContainer() // on page load
+window.onresize = resizeShroomContainer //on window resize
 
 // Load shroom
 getShroomFromDB(shroomId)
@@ -19,7 +19,7 @@ getShroomFromDB(shroomId)
   })
 })
 
-function resizeLargeShroomContainer() {
+function resizeShroomContainer() {
   const navbar = document.querySelector('.navbar')
   let winHeight = window.innerHeight - navbar.clientHeight
   let width = winHeight / 1.6
