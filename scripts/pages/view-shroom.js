19cbcf0654a6664c7176d3e63854ee82d4cb7bd3
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 21:48:50 -0800

    Hide delete button if shroom is not 'owned'.

--- scripts/pages/view-shroom.js
+++ scripts/pages/view-shroom.js
@@ -1,6 +1,14 @@
 const shroomId = getShroomIdFromURL()
 const container = document.querySelector('#shroom-view')
 
+// Button Listeners
+const share = document.querySelector('#share')
+const edit = document.querySelector('#edit')
+const del = document.querySelector('#delete')
+share.addEventListener('click', () => {})
+edit.addEventListener('click', () => window.location=`edit.html#/shroom/${shroomId}`)
+del.addEventListener('click', () => deleteShroom(shroomId))
+
 // Set shroom card image size
 resizeShroomContainer() // on page load
 window.onresize = resizeShroomContainer //on window resize
@@ -11,21 +19,16 @@ getShroomFromDB(shroomId)
   attachParts(shroom)
   .then(result => {
     container.innerHTML = makeShroomCard(result)
-    if (shroomUser.id !== shroom.owner_id) { //add badge
+    if (shroomUser.id !== shroom.owner_id) { 
+      // Add badge
       const shroomCard = document.querySelector('.shroom-card')
       const badge = makeSharedBadge()
       shroomCard.appendChild(badge)
+      // Hide delete button
+      del.style.display = 'none'
     }
   })
 })
-
-// Button Listeners
-const share = document.querySelector('#share')
-const edit = document.querySelector('#edit')
-const del = document.querySelector('#delete')
-share.addEventListener('click', () => {})
-edit.addEventListener('click', () => window.location=`edit.html#/shroom/${shroomId}`)
-del.addEventListener('click', () => deleteShroom(shroomId))
 
 function resizeShroomContainer() {
   const navbar = document.querySelector('.navbar')
